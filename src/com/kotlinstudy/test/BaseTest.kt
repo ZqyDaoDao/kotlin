88d430d37d58d88ce775478c1fbefc12a961a12c
zhangqiuyu <zhangqiuyu@syscloub.cn>
2018-02-05 17:51:28 +0800

    commmit

---
+++ src/com/kotlinstudy/test/BaseTest.kt
@@ -0,0 +1,52 @@
+package com.kotlinstudy.test
+
+fun parseInt(str: String): Int? {
+    return str.toIntOrNull()
+}
+
+fun printProduct(arg1: String,arg2: String){
+    val x = parseInt(arg1)
+    val y = parseInt(arg2)
+
+    if(x == null){
+        println("Wrong number format in arg1: '${arg1}'")
+        return
+    }
+
+    if(y == null){
+        println("Wrong number format in arg2: '${arg2}'")
+        return
+    }
+
+    println(x * y)
+}
+
+fun main(args: Array<String>){
+    println("hello, world")
+
+    printProduct("6","7")
+    printProduct("a","7")
+    printProduct("9","b")
+
+    var items = listOf<String>("apple", "banana", "kiwi")
+
+    for(item in items){
+        println(item)
+    }
+
+    var x= 10
+    var y = 9
+    if(x in 1..y+1){
+        println("fits in range")
+    }
+
+    for(x in 1..5){
+        print(x)
+    }
+
+    val fruits = listOf("banana", "avocado", "apple", "kiwi")
+    fruits.filter { it.startsWith("a") }
+            .sortedBy { it }
+            .map { it.toUpperCase() }
+            .forEach { println(it) }
+}
